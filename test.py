fea8d3ab41bc42d4add5d6c63b362458d720f4e5
bbooze1 <bryce.booze@equitymethods.com>
2023-04-30 00:50:25 -0700

    added check for chacha during keygen

--- test.py
+++ test.py
@@ -4,9 +4,9 @@ from encrypt_class import enc_object
 
 
 def AES_GCM_Test():
-    byte_lengths = [16, 32]
-    aes_mode = "AES-GCM".encode()
-    plaintexts = ["This is a AES-128-GCM test", "This is a AES-256-GCM test"]
+    byte_lengths = [32]
+    aes_mode = "ChaCha20_Poly1305".encode()
+    plaintexts = ["T"]
 
 
     for byte_length, data in zip(byte_lengths, plaintexts):
@@ -28,34 +28,9 @@ def AES_GCM_Test():
         assert plaintext == data
 
 
-# def AES_CBC_Test():
-#     byte_lengths = [16, 24, 32]
-#     aes_mode = b"AES-CBC"
-#     plaintexts = ["This is a AES-128-CBC test", "This is a AES-192-CBC test", "This is a AES-256-CBC test"]
-
-#     for byte_length, data in zip(byte_lengths, plaintexts):
-#         # generate a new symmetric key for the session and send it to the receiver using the public key
-#         aes_key = key_generation.aes_key_generation(byte_length, aes_mode)
-#         enc_aes_key_mode = ciphers.encrypt_symmetric_key_mode('public.pem', aes_key, aes_mode)
-
-#         # the receiver will then decrypt it using their private key and use the symmetric key for the rest of the session
-#         aes_key, aes_mode = ciphers.decrypt_symmetric_key_mode('private.pem', enc_aes_key_mode)
-
-#         # run multiple times to simulate 100 messages being sent in the same session
-#         for _ in range(100):
-#             # sender will encrypt the message using the symmetric key and specific the encryption algorithm
-#             enc_data = ciphers.encrypt_message(aes_key, aes_mode, data)
-
-#             # receiver will decrypt the message using the symmetric key and specified encryption algorithm
-#             plaintext = ciphers.decrypt_message(enc_data, aes_key, aes_mode)
-#         assert plaintext == data
-
-
-
 if __name__ == "__main__":
     # every time a new session is started first generate a new asymmetric key pair
     # The public key should be shared with the sender while the private key stays with the receiver
     message = generate_rsa_keys()
     print(message)
     AES_GCM_Test()
-    # AES_CBC_Test()
