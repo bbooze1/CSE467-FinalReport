a5707d9bd6ddb12b01fcf7d1f627d41b0658ddbc
bbooze1 <bryce.booze@equitymethods.com>
2023-04-30 00:15:02 -0700

    combined encryped data

--- encrypt_class.py
+++ encrypt_class.py
@@ -1,5 +1,3 @@
 class enc_object(object):
-    def __init__(self, new_key, new_mode, new_data):
-        self.aes_key = new_key
-        self.mode = new_mode
-        self.data = new_data
+    def __init__(self, new_enc_data):
+        self.enc_data = new_enc_data
