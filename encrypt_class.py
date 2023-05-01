916ec28aac986c094373ac5acac3835f2a261cd9
bbooze1 <bryce.booze@equitymethods.com>
2023-04-30 21:29:03 -0700

    Documentation

--- encrypt_class.py
+++ encrypt_class.py
@@ -1,5 +1,21 @@
 class enc_object():
-    def __init__(self, new_key, new_mode, new_data):
+
+    def __init__(
+        self, 
+        new_key: bytes, 
+        new_mode: bytes, 
+        new_data: bytes,
+    ) -> None:
+        """Class usable to combine encrypted key, mode, and data to send to server
+
+        Args:
+            new_key: encrypted aes_key
+            new_mode: encrypted aes_mode
+            new_data: encrypted data
+        Returns:
+            None
+        """
+        
         self.aes_key = new_key
         self.mode = new_mode
         self.data = new_data
